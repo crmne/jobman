d416503debb690ef74a8ec3e871ce970fcb3b0ef
James Bergstra <bergstrj@iro.umontreal.ca>
2008-05-28 17:31:43 -0400

    rev

--- api0.py
+++ api0.py
@@ -4,46 +4,71 @@ class DbHandle (object):
     This class also provides filtering shortcuts that hide the names of the
     DbHandle internal databases.
 
+    Attributes:
+    dictid_table
+    keyval_table
+    member_table
+
+
+
+    dictid_table
+
+        An SqlAlchemy-mapped class corresponding to database table with the
+        following schema:
+
+            Column('id', Integer, primary_key=True)
+            Column('create_date', DateTime)
+            Column('write_date', DateTime)
+            Column('read_date', DateTime)
+
+    keyval_table
+
+        An SqlAlchemy-mapped class corresponding to database table with the
+        following schema:
+
+            Column('id', Integer, primary_key=True)
+            Column('name', String(128))
+            Column('ival', Integer)
+            Column('fval', Double)
+            Column('cval', Complex)
+            Column('sval', Text))
+            Column('bval', Binary)  
+
+    member_table
+
+        An SqlAlchemy-mapped class corresponding to database table with the
+        following schema:
+
+            Column('id', Integer, primary_key=True)
+
+            Column('trial_id', Integer, ForeignKey('%s.id' % t_trial)),
+            Column('keyval_id', Integer, ForeignKey('%s.id' % t_keyval)))
+
+            TODO: Consider making ('trial_id', 'keyval_id') a joint primary key
     """
 
-    DictIdTable = None
-    """
-    An SqlAlchemy-mapped class corresponding to database table with the
-    following schema:
+    class Dict (object):
+        """
+        Instances are dict-like objects with additional features for communicating
+        with an active database handle.
 
-        Column('id', Integer, primary_key=True)
-        Column('create_date', DateTime)
-        Column('write_date', DateTime)
-        Column('read_date', DateTime)
-    
-    """
+        """
+        def __init__(self, handle):
+            self.handle = handle
 
-    KeyValTable = None
-    """
-    An SqlAlchemy-mapped class corresponding to database table with the
-    following schema:
+        def reload(): 
+            """Sync key-value pairs from database to self"""
+        def save():
+            """Sync key-value pairs from self to database"""
+            pass
+        def delete(self):
+            pass
 
-        Column('id', Integer, primary_key=True)
-        Column('name', String(128))
-        Column('ival', Integer)
-        Column('fval', Double)
-        Column('cval', Complex)
-        Column('sval', Text))
-        Column('bval', Binary)  
-    """
-
-    MemberTable = None
-    """
-    An SqlAlchemy-mapped class corresponding to database table with the
-    following schema:
-
-        Column('id', Integer, primary_key=True)
-
-        Column('trial_id', Integer, ForeignKey('%s.id' % t_trial)),
-        Column('keyval_id', Integer, ForeignKey('%s.id' % t_keyval)))
-
-        TODO: Consider making ('trial_id', 'keyval_id') a joint primary key
-    """
+    def __init__(self, dictid_table, keyval_table, member_table):
+        self.dictid_table = dictid_table
+        self.keyval_table = keyval_table
+        self.member_table = member_table
+        
 
     def insert(self, dct):
         """
@@ -68,18 +93,4 @@ class DbHandle (object):
     def with_cond(self, todo):
         pass
 
-class DbDict (object):
-    """
-    Instances are dict-like objects with additional features for communicating
-    with an active database handle.
-
-    """
-
-    def reload(): 
-        """Sync key-value pairs from database to self"""
-    def save():
-        """Sync key-value pairs from self to database"""
-        pass
-    def delete(self):
-        pass
 
