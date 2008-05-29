ca4b93e230fc727b989b258d3cee5b797a68dd85
James Bergstra <bergstrj@iro.umontreal.ca>
2008-05-29 19:15:27 -0400

    progress

--- api0.py
+++ api0.py
@@ -1,3 +1,6 @@
+from sqlalchemy.orm import mapper, relation, backref, eagerload
+
+class Todo(Exception): """Replace this with some working code!"""
 
 class DbHandle (object):
     """
@@ -5,75 +8,283 @@ class DbHandle (object):
     DbHandle internal databases.
 
     Attributes:
-    dictid_table
-    keyval_table
-    member_table
+    dict_table
+    pair_table
+    link_table
 
 
 
-    dictid_table
+    dict_table
 
         An SqlAlchemy-mapped class corresponding to database table with the
         following schema:
 
             Column('id', Integer, primary_key=True)
-            Column('create_date', DateTime)
-            Column('write_date', DateTime)
-            Column('read_date', DateTime)
+            Column('create', DateTime)
+            Column('write', DateTime)
+            Column('read', DateTime)
 
-    keyval_table
+            #TODO: reconsider create/read/write
+
+    pair_table
 
         An SqlAlchemy-mapped class corresponding to database table with the
         following schema:
 
             Column('id', Integer, primary_key=True)
             Column('name', String(128))
-            Column('ival', Integer)
+            Column('ntype', Integer)
             Column('fval', Double)
-            Column('cval', Complex)
             Column('sval', Text))
-            Column('bval', Binary)  
+            Column('bval', Blob)  
 
-    member_table
+            #TODO: Consider difference between text and binary
+            #TODO: Consider adding a 'type' column
+            #TODO: Consider union?
+            #TODO: Are there stanard ways of doing this kind of thing?
+
+    link_table
 
         An SqlAlchemy-mapped class corresponding to database table with the
         following schema:
 
-            Column('id', Integer, primary_key=True)
+            Column('dict_id', Integer, ForeignKey('%s.id' % t_trial), primary_key=True),
+            Column('keyval_id', Integer, ForeignKey('%s.id' % t_keyval), primary_key=True))
 
-            Column('trial_id', Integer, ForeignKey('%s.id' % t_trial)),
-            Column('keyval_id', Integer, ForeignKey('%s.id' % t_keyval)))
-
-            TODO: Consider making ('trial_id', 'keyval_id') a joint primary key
     """
 
-    class Dict (object):
-        """
-        Instances are dict-like objects with additional features for communicating
-        with an active database handle.
+    e_bad_table = 'incompatible columns in table'
 
-        """
-        def __init__(self, handle):
-            self.handle = handle
+    def __init__(h_self, Session, dict_table, pair_table, link_table):
+        h_self._dict_table = dict_table
+        h_self._pair_table = pair_table
+        h_self._link_table = link_table
 
-        def reload(): 
-            """Sync key-value pairs from database to self"""
-        def save():
-            """Sync key-value pairs from self to database"""
-            pass
-        def delete(self):
-            pass
+        #TODO: replace this crude algorithm (ticket #17)
+        if ['id', 'create', 'write', 'read'] != [c.name for c in dict_table.c]:
+            raise ValueError(h_self.e_bad_table, dict_table)
+        if ['id', 'name', 'ntype', 'fval', 'sval', 'bval'] != [c.name for c in pair_table.c]:
+            raise ValueError(h_self.e_bad_table, pair_table)
+        if ['dict_id', 'pair_id'] != [c.name for c in link_table.c]:
+            raise ValueError(h_self.e_bad_table, pair_table)
 
-    def __init__(self, dictid_table, keyval_table, member_table):
-        self.dictid_table = dictid_table
-        self.keyval_table = keyval_table
-        self.member_table = member_table
+        h_self._session = Session()
+
+        class KeyVal (object):
+            def __init__(k_self, name, val):
+                k_self.name = name
+                k_self.val = val
+            def __repr__(k_self):
+                return "<Param(%s,'%s', %s)>" % (k_self.id, k_self.name, repr(k_self.val))
+            def __get_val(k_self):
+                val = None
+                if k_self.fval is not None: val = [int, float][k_self.ntype](k_self.fval)
+                if k_self.bval is not None: val = k_self.bval
+                if k_self.sval is not None: val = k_self.sval
+                return  val
+            def __set_val(k_self, val):
+                if isinstance(val, str):
+                    k_self.fval = None
+                    k_self.bval = None
+                    k_self.sval = val
+                else:
+                    k_self.sval = None
+                    try:
+                        f = float(val)
+                    except (TypeError, ValueError):
+                        f = None
+                    if f is None: #binary data
+                        k_self.bval = val
+                        k_self.fval = None
+                        k_self.ntype = None
+                    else:
+                        k_self.bval = None
+                        k_self.fval = f
+                        k_self.ntype = isinstance(val,float)
+            val = property(__get_val, __set_val)
+
+        mapper(KeyVal, pair_table)
         
+        class Dict (object):
+            """
+            Instances are dict-like objects with additional features for
+            communicating with an active database.
 
-    def insert(self, dct):
+            This class will be mapped by SqlAlchemy to the dict_table.
+
+            Attributes:
+            handle - reference to L{DbHandle} (creator)
+
+            """
+            def __init__(d_self):
+                s = h_self._session
+                s.save(d_self)
+                s.commit()
+
+            _forbidden_keys = set(['session'])
+
+            #
+            # dictionary interface
+            #
+
+            def __contains__(d_self, key):
+                raise Todo
+
+            def __getitem__(d_self, key):
+                raise Todo
+
+            def _set_in_session(d_self, key, val, session):
+                if key in d_self._forbidden_keys:
+                    raise KeyError(key)
+                created = None
+                for i,a in enumerate(d_self._attrs):
+                    if a.name == key:
+                        assert created == None
+                        created = h_self._KeyVal(key, val)
+                        attrlist[i] = created
+                if not created:
+                    created = h_self._KeyVal(key, val)
+                    d_self._attrs.append(created)
+                session.save(created)
+
+            def __setitem__(d_self, key, val):
+                s = h_self._session
+                d_self._set_in_session(ke, val, s)
+                s.update(d_self)
+                s.commit()
+
+
+            def items(d_self):
+                return [(kv.name, kv.val) for kv in d_self._attrs]
+
+            def update(d_self, dct, **kwargs):
+                s = h_self._session
+                for k, v in dct.items():
+                    d_self._set_in_session(k, v, s)
+                for k, v in kwargs.items():
+                    d_self._set_in_session(k, v, s)
+                s.update(d_self)
+                s.commit()
+
+            #
+            # database stuff
+            #
+
+            def refresh(d_self, session=None): 
+                """Sync key-value pairs from database to self
+
+                @param session: use the given session, and do not commit.
+                
+                """
+                if session is None:
+                    session = h_self._session
+                    session.refresh(self.dbrow)
+                    session.commit()
+                else:
+                    session.refresh(self.dbrow)
+
+            def delete(d_self, session=None):
+                """Delete this dictionary from the database
+                
+                @param session: use the given session, and do not commit.
+                """
+                raise Todo
+        mapper(Dict, dict_table,
+                properties = {
+                    '_attrs': relation(KeyVal, 
+                        secondary=link_table, 
+                        cascade="delete-orphan")
+                    })
+
+        class Query (object):
+            """
+            Attributes:
+            _query - SqlAlchemy.Query object
+            """
+
+            def __init__(q_self, query):
+                q_self._query = query
+
+            def __iter__(q_self):
+                return q_self.all().__iter__()
+
+            def filter_by(q_self, **kwargs):
+                """Return a Query object that restricts to dictionaries containing
+                the given kwargs"""
+
+                #Note: when we add new types to the key columns, add them here
+                q = q_self._query
+                T = h_self._Dict
+                for kw, arg in kwargs.items():
+                    if isinstance(arg, str):
+                        q = q.filter(T._attrs.any(name=kw, sval=arg))
+                    else:
+                        try:
+                            f = float(arg)
+                        except (TypeError, ValueError):
+                            f = None
+                        if f is None:
+                            q = q.filter(T._attrs.any(name=kw, bval=arg))
+                        else:
+                            q = q.filter(T._attrs.any(name=kw, fval=f))
+
+                return h_self._Query(q)
+
+            def all(q_self):
+                """Return an iterator over all matching dictionaries.
+
+                See L{SqlAlchemy.Query}
+                """
+                return q_self._query.all()
+
+            def any(q_self):
+                """Return some matching dictionary, or None
+                See L{SqlAlchemy.Query}
+                """
+                return q_self._query.any()
+
+            def all_ordered_by(q_self, key, desc=False):
+                """Return query results, sorted.
+
+                @type key: string or tuple of string or list of string
+                @param: keys by which to sort the results.
+
+                @rtype: list of L{DbHandle._Dict} instances
+                @return: query results, sorted by given keys
+                """
+
+                # order_by is not easy to do in SQL based on the data structures we're
+                # using.  Considering we support different data types, it may not be
+                # possible at all.
+                #
+                # It would be easy if 'pivot' or 'crosstab' were provided as part of the
+                # underlying API, but they are not. For example, read this:
+                # http://www.simple-talk.com/sql/t-sql-programming/creating-cross-tab-queries-and-pivot-tables-in-sql/
+
+                # load query results
+                results = list(q_self.all())
+
+                if isinstance(key, (tuple, list)):
+                    val_results = [([d[k] for k in key], d) for d in results]
+                else:
+                    val_results = [(d[key], d) for d in results]
+
+                val_results.sort() #interesting: there is an optional key parameter
+                if desc:
+                    val_results.reverse()
+                return [vr[-1] for vr in val_results]
+
+        h_self._KeyVal = KeyVal
+        h_self._Dict = Dict
+        h_self._Query = Query
+
+    def __iter__(h_self):
+        return h_self.query().__iter__()
+
+    def insert(h_self, **dct):
         """
-        @rtype:  DbDict with reference to self
-        @return: a DbDict initialized as a copy of dct
+        @rtype:  DbHandle with reference to self
+        @return: a DbHandle initialized as a copy of dct
         
         @type dct: dict-like instance whose keys are strings, and values are
         either strings, integers, floats
@@ -81,16 +292,16 @@ class DbHandle (object):
         @param dct: dictionary to insert
 
         """
-    def query(self, session): 
+        rval = h_self._Dict()
+        rval.update(dct)
+        return rval
+
+    def query(h_self, **kwargs): 
         """Construct an SqlAlchemy query, which can be subsequently filtered
-        using the filtering classmethods of DbDict"""
-        return session.Query(self.DictTable)
-        pass
+        using the instance methods of DbQuery"""
 
-    def with_any(self, kw, arg):
-        pass
-
-    def with_cond(self, todo):
-        pass
+        return h_self._Query(h_self._session.query(h_self._Dict)\
+                        .options(eagerload('_attrs')))\
+                        .filter_by(**kwargs)
 
 
---
+++ test_api0.py
@@ -0,0 +1,113 @@
+from api0 import *
+import threading, time, commands, os, sys, math, random, datetime
+
+import psycopg2, psycopg2.extensions
+from sqlalchemy import create_engine, desc
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Table, Column, MetaData, ForeignKey    
+from sqlalchemy import Integer, String, Float, DateTime, Text, Binary
+from sqlalchemy.orm import mapper, relation, backref, eagerload
+from sqlalchemy.sql import operators, select
+
+import unittest
+
+class T(unittest.TestCase):
+
+    def test_bad_dict_table(self):
+        """Make sure our crude version of schema checking kinda works"""
+        engine = create_engine('sqlite:///:memory:', echo=False)
+        Session = sessionmaker(bind=engine, autoflush=True, transactional=True)
+
+        table_prefix='bergstrj_scratch_test_'
+        metadata = MetaData()
+        t_trial = Table(table_prefix+'trial', metadata,
+                Column('id', Integer, primary_key=True),
+                Column('desc', String(256)), #comment: why running this trial?
+                Column('priority', Float(53)), #aka Double
+                Column('start', DateTime),
+                Column('finish', DateTime),
+                Column('host', String(256)))
+        metadata.create_all(engine)
+
+        try:
+            h = DbHandle(None, t_trial, None, None)
+        except ValueError, e:
+            if e[0] == DbHandle.e_bad_table:
+                return
+        self.fail()
+
+
+    def go(self):
+        engine = create_engine('sqlite:///:memory:', echo=False)
+        Session = sessionmaker(bind=engine, autoflush=True, transactional=True)
+
+        table_prefix='bergstrj_scratch_test_'
+        metadata = MetaData()
+
+        t_trial = Table(table_prefix+'trial', metadata,
+                Column('id', Integer, primary_key=True),
+                Column('create', DateTime),
+                Column('write', DateTime),
+                Column('read', DateTime))
+
+        t_keyval = Table(table_prefix+'keyval', metadata,
+                Column('id', Integer, primary_key=True),
+                Column('name', String(32), nullable=False), #name of attribute
+                Column('ntype', Integer),
+                Column('fval', Float(53)), #aka Double
+                Column('sval', Text), #aka Double
+                Column('bval', Binary)) #TODO: store text (strings of unbounded length)
+
+        t_trial_keyval = Table(table_prefix+'trial_keyval', metadata,
+                Column('dict_id', Integer, ForeignKey('%s.id' % t_trial),
+                    primary_key=True),
+                Column('pair_id', Integer, ForeignKey('%s.id' % t_keyval),
+                    primary_key=True))
+
+        metadata.create_all(engine) # does nothing when tables already exist
+        
+        return Session, t_trial, t_keyval, t_trial_keyval
+
+
+    def test_insert_save(self):
+
+        Session, t_dict, t_pair, t_link = self.go()
+
+        db = DbHandle(*self.go())
+
+        def jobs():
+            dvalid, dtest = 'dvalid', 'dtest file'
+            desc = 'debugging'
+            for lr in [0.001]:
+                for scale in [0.0001 * math.sqrt(10.0)**i for i in range(4)]:
+                    for rng_seed in [4, 5, 6]:
+                        for priority in [None, 1]:
+                            yield dict(locals())
+
+        jlist = list(jobs())
+        assert len(jlist) == 1*4*3*2
+        for i, dct in enumerate(jobs()):
+            t = db.insert(**dct)
+
+        q0list = list(db.query().all())
+        q1list = list(db.query())
+        q2list = list(db)
+
+        self.failUnless(q0list == q1list, (q0list,q1list))
+        self.failUnless(q0list == q2list, (q0list,q1list))
+
+        self.failUnless(len(q0list) == len(jlist))
+
+        for i, (j, q) in enumerate(zip(jlist, q0list)):
+            jitems = list(j.items())
+            qitems = list(q.items())
+            jitems.sort()
+            qitems.sort()
+            if jitems != qitems:
+                print i
+                print jitems
+                print qitems
+            self.failUnless(jitems == qitems, (jitems, qitems))
+
+if __name__ == '__main__':
+    unittest.main()
