4b4fb84cf97bcb473648a9fa05d9bf8307dad99a
James Bergstra <bergstrj@iro.umontreal.ca>
2009-09-09 18:51:44 -0400

    added comment to def_addition

--- jobman/examples/def_addition.py
+++ jobman/examples/def_addition.py
@@ -1,3 +1,8 @@
+""" Example library file implementing an experiment.
+"""
+
+# Typically this sort of file would be a lot more complicated, and deserve it's own file.
+
 def addition_example(state, channel):
 
     print 'state.first =', state.first
