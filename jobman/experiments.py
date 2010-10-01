ff01bf7b92e342ad55ff6a28987668676d700674
Frederic Bastien <nouiz@nouiz.org>
2010-10-01 16:23:41 -0400

    added an experiment that allow to test different return case of the experiment.

--- jobman/experiments.py
+++ jobman/experiments.py
@@ -1,4 +1,5 @@
 #This file contain example experiment and in the futur some generic experiment(for example for PLearn)
+import sys
 
 def example1(state, channel):
 
@@ -22,5 +23,23 @@ def example2(state, channel):
     f.write("some results!")
     f.close()
     return channel.COMPLETE
+
+
+def theano_test_return(state, channel):
+    """
+    This experiment allow to test different test case
+
+    state param used wanted_return
+    """
+    state.will_return = state.wanted_return
+    if state.wanted_return == 'raise':
+        raise Exception("This jobs raise an error")    
+    elif state.wanted_return == 'incomplete':
+        return channel.INCOMPLETE
+    elif state.wanted_return == 'complete':
+        return channel.COMPLETE
+    elif state.wanted_return == 'sysexit':
+        sys.exit()
+
 
 
