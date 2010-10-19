142fe5950751cf358c6626d0348074da374f44e2
Frederic Bastien <nouiz@nouiz.org>
2010-10-19 16:34:01 -0400

    added a setup file to install it correctly.

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#
+#  TODO:
+#   * Figure out how to compile and install documentation automatically
+#   * Add back in installation requirements
+#   * Add download_url
+
+
+from distutils.core import setup
+
+setup(name='Jobman',
+      version='hg',
+      description='Facilitate handling of many jobs(especially jobs send on cluster)',
+      author='LISA laboratory, University of Montreal',
+      author_email='theano-user@googlegroups.com',
+      url='http://www.deeplearning.net/software/jobman',
+      packages=['jobman', 'jobman.examples','jobman.analyze'
+                ],
+      scripts=['bin/jobman']
+
+           )
