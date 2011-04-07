3f9fcf2d1069cc2cac75177b02010e3c27727282
Frederic Bastien <nouiz@nouiz.org>
2011-04-07 16:14:10 -0400

    small fix.

--- jobman/dbi/dbi.py
+++ jobman/dbi/dbi.py
@@ -115,13 +115,13 @@ class MultiThread:
         if maxThreads==-1:
             nb_thread=len(argsVector)
         elif isinstance(maxThreads,str):
-            self._lock_threadPool = Lock()
             self.maxThreads_file = maxThreads
             nb_thread = 0#Thread will be created when self.start() is called.
         elif maxThreads<=0:
             raise DBIError("[DBI] ERROR: you set %d concurrent jobs. Must be higher then 0!!"%(maxThreads))
         else:
             nb_thread=maxThreads
+        self._lock_threadPool = Lock()
         if nb_thread>len(argsVector):
             nb_thread=len(argsVector)
 
