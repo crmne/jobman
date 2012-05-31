d505a3feabd2fa76cdd19411114da54c8eaebb24
Frederic Bastien <nouiz@nouiz.org>
2012-05-31 09:55:28 -0400

    make jobman check work on PBS/TORQUE

--- jobman/check.py
+++ jobman/check.py
@@ -1,5 +1,6 @@
 from subprocess import Popen, PIPE
 import os
+import re
 import time
 from optparse import OptionParser
 
@@ -10,6 +11,62 @@ from api0 import open_db
 
 parse_check = OptionParser(usage='%prog check <tablepath> ',
                            add_help_option=False)
+
+
+def str_time(run_time):
+    run_time = "%dd %dh%dm%ds" % (run_time / (24 * 3600),
+                                  run_time % (24 * 3600) / 3600,
+                                  run_time % 3600 / 60,
+                                  run_time % 60)
+    return run_time
+
+
+def check_running_pbs_jobs(r, now):
+    """ Verify jobs on Torque/PBS system"""
+    p = Popen('qstat -x %s' % r['jobman.sql.pbs_task_id'],
+              shell=True, stdout=PIPE, stderr=PIPE)
+    ret = p.wait()
+    out = p.stdout.read()
+
+    if len(out) == 0:
+        print ("E: Job %d marked as PBS job '%s',"
+               " but 'qstat' don't know it." % (
+                  r.id, r['jobman.sql.pbs_task_id']))
+        return
+
+    run_time = str_time(now - r["jobman.sql.start_time"])
+
+    #check runtime
+    #<walltime>48:00:00</walltime>
+    ressource_str = re.search('<Resource_List>.*</Resource_List>',
+                              out).group(0)
+    walltime_str = re.search('<walltime>.*?</walltime>', ressource_str)
+    walltime_str = walltime_str.group(0)[10:-11]
+    assert walltime_str[2] == ':' and walltime_str[5] == ':'
+    walltime = (int(walltime_str[:2]) * 60 * 60 +
+                int(walltime_str[3:5]) * 60 +
+                int(walltime_str[-2:]))
+    if now - int(r["jobman.sql.start_time"]) > walltime:
+        print ("W: Job %d is running for more then the specified"
+               " max time of %s. Run time %s" % (
+                   r.id, walltime_str, run_time))
+
+    # check state
+    #<job_state>R</job_state>
+    state_str = re.search('<job_state>.*</job_state>', out).group(0)[11:-12]
+    if state_str == "R":
+        pass
+    elif state_str == "Q":
+        print ("E: Job %d is running in the db on pbs with job id %s, but it"
+               " is in the pbs queue. Run time %s" % (
+                  r.id, r["jobman.sql.pbs_task_id"], run_time))
+    elif state_str == "C":
+        print ("W: Job %d is running in the db, but it is marked as completed"
+               " in the pbs queue. This can be synchonization issue. Retry in"
+               " 1 minutes. Run time %s." % (r.id, run_time))
+    else:
+        print ("W: Job %d is running in the db, but we don't understand the"
+               " state in the queue '%s'" % (r.id, state_str))
 
 
 def check_serve(options, dbdescr):
@@ -62,18 +119,11 @@ def check_serve(options, dbdescr):
         host_slot = {}
         now = time.time()
 
-        def str_time(x):
-            run_time = now - x
-            run_time = "%dd %dh%dm%ds" % (run_time / (24 * 3600),
-                                          run_time % (24 * 3600) / 3600,
-                                          run_time % 3600 / 60,
-                                          run_time % 60)
-            return run_time
-
         #check job still running
         for idx, r in enumerate(running):
             condor_job = False
             sge_job = False
+            pbs_job = False
 
             #find the backend used for the job.
             if ("jobman.sql.condor_slot" in r.keys() and
@@ -81,11 +131,13 @@ def check_serve(options, dbdescr):
                 condor_job = True
             if "jobman.sql.sge_task_id" in r.keys():
                 sge_job = True
-            if sge_job and condor_job:
-                print "W: Job %d have info such that it run on condor and on sge. We can't determine the good one."
+            if "jobman.sql.pbs_task_id" in r.keys():
+                pbs_job = True
+            if (sge_job + condor_job + pbs_job) > 1:
+                print "W: Job %d have info such that it run on condor, sge and/or pbs. We can't determine the good one."
                 continue
-            if not (sge_job or condor_job):
-                print "W: Job %d don't have sge or condor info attached to it. We can't determine if it is still running on the cluster. Old jobman to start the job?"%r.id
+            if not (sge_job or condor_job or pbs_job):
+                print "W: Job %d don't have condor, sge or pbs info attached to it. We can't determine if it is still running on the cluster. Old jobman to started the job?" % r.id
                 continue
 
             #check that the job is still running.
@@ -110,7 +162,7 @@ def check_serve(options, dbdescr):
                 #assert lines[1]=='-----------------------------------------------------------------------------------------------------------------------------------------------------------------------\n'
                 assert lines[0]=='job-ID  prior   name       user         state submit/start at     queue                          slots ja-task-ID \n'
                 assert lines[1]=='-----------------------------------------------------------------------------------------------------------------\n'
-                run_time = str_time(r["jobman.sql.start_time"])
+                run_time = str_time(now - r["jobman.sql.start_time"])
                 if now - int(r["jobman.sql.start_time"]) > (24 * 60 * 60):
                     print "W: Job %d is running for more then 24h. The current colosse max run time is 24h. Run time %s"%(r.id,run_time)
 
@@ -137,6 +189,9 @@ def check_serve(options, dbdescr):
                     print "E: Job %d marked as running in the db on sge with job id %s and task id %s, but not in sge queue. Run time %s."%(r.id,r["jobman.sql.job_id"],r["jobman.sql.sge_task_id"],run_time)
                 continue
 
+            if pbs_job:
+                check_running_pbs_jobs(r, now)
+                continue
             try:
                 h = r["jobman.sql.host_name"]
                 s = r["jobman.sql.condor_slot"]
@@ -146,11 +201,11 @@ def check_serve(options, dbdescr):
             st = s + '@' + h
             if host_slot.has_key(st):
                 try:
-                    t0 = str_time(running[host_slot[st]]["jobman.sql.start_time"])
+                    t0 = str_time(now - running[host_slot[st]]["jobman.sql.start_time"])
                 except KeyError:
                     t0 = 'NO_START_TIME'
                 try:
-                    t1 = str_time(r["jobman.sql.start_time"])
+                    t1 = str_time(now - r["jobman.sql.start_time"])
                 except KeyError:
                     t1 = 'NO_START_TIME'
                 print 'E: Job %d and Job %d are running on the same condor slot/host combination. running time: %s and %s'%(running[host_slot[st]].id,r.id,t0,t1)
@@ -206,7 +261,7 @@ def check_serve(options, dbdescr):
                     r["jobman.sql.condor_slot"],
                     r["jobman.sql.host_name"],
                     r["jobman.sql.start_time"])
-            run_time = str_time(info[4])
+            run_time = str_time(now - info[4])
 
             if info[2] == "no_condor_slot":
                 print "W: Job %d is not running on condor(Should not happed...)"%info[0]
